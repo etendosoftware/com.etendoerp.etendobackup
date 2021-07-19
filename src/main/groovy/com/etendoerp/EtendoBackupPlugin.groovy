f12695f758ec4fb3ccfcb7f984ae8c3d788f6b09
Pablo Elena <pablo.elena@smfconsulting.es>
2021-07-19 10:22:46 -0300

    Loading restore module tasks

--- src/main/groovy/com/etendoerp/EtendoBackupPlugin.groovy
+++ src/main/groovy/com/etendoerp/EtendoBackupPlugin.groovy
@@ -1,6 +1,7 @@
 package com.etendoerp
 
 import com.etendoerp.backup.BackupModule
+import com.etendoerp.restore.RestoreModule
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,5 +9,6 @@ public class EtendoBackupPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         BackupModule.load(project)
+        RestoreModule.load(project)
     }
 }
